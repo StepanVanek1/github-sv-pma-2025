353ccf90ac315a6b4de37e89f969270c68dc5bf7
Štěpán Vaněk <stepan.vanek@actis.cz>
2025-10-06 18:36:58 +0200

    fix

--- MyApp002MyOwnConstraintLayout/app/src/main/java/com/example/myapp002myownconstraintlayout/MainActivity.kt
+++ MyApp002MyOwnConstraintLayout/app/src/main/java/com/example/myapp002myownconstraintlayout/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapp002myownconstaraintlayout
+package com.example.myapp002myownconstraintlayout
 
 import android.annotation.SuppressLint
 import android.os.Bundle
@@ -6,10 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
